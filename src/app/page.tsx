d151f2c3fb0eb688bd63d7d8a3ddfa5dc62d8f91
João Inácio Neto <birobirobiro.dev@gmail.com>
2025-02-18 21:17:42 -0300

    Aula 02

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+const ProductPage = () => {
+  return (
+    <div className="p-5 border-red-500 border-2 rounded-xl">
+      <h1 className="font-bold">Products Page</h1>
+    </div>
+  )
+};
+
+export default ProductPage;
